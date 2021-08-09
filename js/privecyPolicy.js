749cece83185b7701f4a0421ab567dbcb64cc4bc
narumi <flaming.june123@gmail.com>
2021-08-09 20:04:01 +0900

    20210809_2003

--- js/privecyPolicy.js
+++ js/privecyPolicy.js
@@ -4,23 +4,12 @@
 $(function() {
     //始めにjQueryで送信ボタンを無効化する
     $('.p-form__submit').prop("disabled", true);
-    
-    //始めにjQueryで必須欄を加工する
-    //$('form input:required').each(function () {
-        //$(this).prev("label").addClass("required");
-    //});
-    
-    //入力欄の操作時
-    $('form input:required').change(function () {
-        //必須項目が空かどうかフラグ
-        let flag = true;
-        //必須項目をひとつずつチェック
-        $('form input:required').each(function(e) {
-            //もし必須項目が空なら
-            if ($('form input:required').eq(e).val() === "") {
+    let flag = true;
+    //プライバシーポリシーがチェックされていたら
+    $('#privacyPolicy--agree').on('click', function() {
+        if ( $(this).prop('checked') == false ) {
                 flag = false;
             }
-        });
         //全て埋まっていたら
         if (flag) {
             //送信ボタンを復活
